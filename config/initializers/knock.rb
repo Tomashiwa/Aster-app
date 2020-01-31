440e5c7f552ddcb9def95c1799573db45c4dc310
Tomashiwa <wongqinliang@gmail.com>
2020-02-01 00:25:40 +0800

    Modified Knock.rb configuration

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -36,7 +36,7 @@ Knock.setup do |config|
   ## Configure the key used to sign tokens.
   ##
   ## Default:
-  # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
 
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
@@ -56,4 +56,26 @@ Knock.setup do |config|
   ##
   ## Default:
   # config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
+end
+
+Knock::AuthTokenController.class_eval do
+  require 'pp'
+
+  private
+    def entity
+      @entity ||=
+        if entity_class.respond_to? :from_token_request
+          entity_class.from_token_request request
+        else
+          # entity_class.find_by username: auth_params[:username]
+          # puts "auth_params"
+          # pp auth_params.to_unsafe_h
+          entity_class.find_by name: auth_params[:name]
+        end
+    end
+
+    def auth_params
+      params.require(:auth).permit :name, :password
+      # params.permit auth: [:username, :password]
+    end
 end
